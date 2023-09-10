ac9b945f8cb44fefbd5c3e7641dd277441562ba5
Abhay12-bit <abhaythakur50174@gmail.com>
2023-09-10 16:45:29 +0530

    Second Commite

---
+++ src/test/java/com/login/SempSun/SempSunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.SempSun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SempSunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
