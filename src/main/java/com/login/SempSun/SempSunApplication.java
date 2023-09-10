7e777b98fc7f85c8e885ace1ef150da113bfed54
Abhay12-bit <abhaythakur50174@gmail.com>
2023-09-10 16:38:43 +0530

    fist

---
+++ src/main/java/com/login/SempSun/SempSunApplication.java
@@ -0,0 +1,13 @@
+package com.login.SempSun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SempSunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SempSunApplication.class, args);
+	}
+
+}
